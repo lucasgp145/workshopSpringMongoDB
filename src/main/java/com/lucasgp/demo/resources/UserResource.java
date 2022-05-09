543eae4b83a6147e60ae280cc57bd91d7dd98b8e
lucasgp145 <lucaspereiravds@gmail.com>
2022-05-09 14:59:34 -0300

    usando padrao dto para retornar usuario e senha

--- src/main/java/com/lucasgp/demo/resources/UserResource.java
+++ src/main/java/com/lucasgp/demo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lucasgp.demo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasgp.demo.domain.User;
+import com.lucasgp.demo.dto.UserDTO;
 import com.lucasgp.demo.services.UserService;
 
 @RestController
@@ -19,11 +21,13 @@ public class UserResource {
 	@Autowired private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public  ResponseEntity<List<User>> findAll(){
+	public  ResponseEntity<List<UserDTO>> findAll(){
+		
 		
 		List<User> getUsers = service.findAll();
+		List<UserDTO> listDto = getUsers.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
-		return  ResponseEntity.ok(getUsers);
+		return  ResponseEntity.ok(listDto);
 	}
 	
 }
